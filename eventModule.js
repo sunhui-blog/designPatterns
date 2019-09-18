43681c752dcb38646b21c11de36cf42de53d98e7
sunhui <2923132761@qq.com>
2019-09-18 20:18:52 +0800

    feat:event module

---
+++ eventModule.js
@@ -0,0 +1,41 @@
+class Events {
+  constructor () {
+    this.store = {}
+    this.onceKey = []
+  }
+
+  // 订阅事件
+  on (key, fn) {
+    !this.store[key] && (this.store[key] = [])
+    this.store[key].push(fn)
+  }
+
+  // 取消事件
+  off (key) {
+    this.store[key] && this.store[key] && (this.store[key] = [])
+  }
+
+  // 事件单次执行
+  one (key, fn) {
+    !this.store[key] && this.on(key, fn)
+    this.onceKey.push(key)
+  }
+
+  // 发布事件
+  emit (key, ...args) {
+    if (this.store[key]) {
+      this.store[key].forEach(fn => fn.apply(null, args))
+      if (this.onceKey.includes(key)) {
+        this.store[key] = []
+        this.onceKey = this.onceKey.filter(item => item !== key)
+      }
+    }
+  }
+}
+
+const event = new Events()
+
+
+event.on('sayHello', () => { console.log('hello') })
+
+event.emit()
